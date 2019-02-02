c7ab026b5ac11948f75dc60186ea8d91d9b15d1c
bukajj <42746240+bukajj@users.noreply.github.com>
2019-02-02 23:36:16 +0100

    TicTacToe.1.0.5
    Ready Game

--- TicTacToeGame/TicTacToeGame/TicTacToeGame.py
+++ TicTacToeGame/TicTacToeGame/TicTacToeGame.py
@@ -1,18 +1,14 @@
-class TicTacToeGame:
+def play():
+    field=[['+',0,1,2],[0,' ',' ',' '],[1,' ',' ',' '],[2,' ',' ',' ']]
     
-    def  __init__(self, player1, player2):
-        self.__player1=player1
-        self.__player2=player2
-        self.__field=[['+',0,1,2],[0,' ',' ',' '],[1,' ',' ',' '],[2,' ',' ',' ']]
+    def tab_print():
+        for tup in field:
+            print(str(tup[0]) + ' ' + str(tup[1]) + ' ' + str(tup[2]) + ' ' + str(tup[3]))
     
-    def __print():
-        for tup in __field:
-            print(tup[0] + ' ' + tup[1] + ' ' + tup[2] + ' ' + tup[3])
-    
-    def __set(value, position):
-        if 0<=position[0]<=2 and 0<=position[1]<=2 and (value=='x' or value=='o'):
-            if __field[position[0]+1][position[1]+1]==' ':
-                __field[position[0]+1][position[1]+1]=value
+    def set(value, position):
+        if 0<=int(position[0])<=2 and 0<=int(position[1])<=2 and (value=='x' or value=='o'):
+            if field[position[0]+1][position[1]+1]==' ':
+                field[position[0]+1][position[1]+1]=value
                 return True
             else:
                 return False
@@ -20,47 +16,61 @@ class TicTacToeGame:
             return False
 
 
-    def __win():
-        if __field[1][1]==__field[1][2]==__field[1][3] and __field[1][1]!=' ':
+    def win():
+        if field[1][1]==field[1][2]==field[1][3] and field[1][1]!=' ':
             return True
-        elif __field[1][1]==__field[2][1]==__field[3][1] and __field[1][1]!=' ':
+        elif field[1][1]==field[2][1]==field[3][1] and field[1][1]!=' ':
             return True
-        elif __field[1][1]==__field[2][2]==__field[3][3] and __field[1][1]!=' ':
+        elif field[1][1]==field[2][2]==field[3][3] and field[1][1]!=' ':
             return True
-        elif __field[1][3]==__field[2][2]==__field[3][1] and __field[1][3]!=' ':
+        elif field[1][3]==field[2][2]==field[3][1] and field[1][3]!=' ':
             return True
-        elif __field[1][2]==__field[2][2]==__field[3][2] and __field[1][2]!=' ':
+        elif field[1][2]==field[2][2]==field[3][2] and field[1][2]!=' ':
             return True
-        elif __field[1][3]==__field[2][3]==__field[3][3] and __field[1][3]!=' ':
+        elif field[1][3]==field[2][3]==field[3][3] and field[1][3]!=' ':
             return True
-        elif __field[2][1]==__field[2][2]==__field[2][3] and __field[2][1]!=' ':
+        elif field[2][1]==field[2][2]==field[2][3] and field[2][1]!=' ':
             return True
-        elif __field[3][1]==__field[3][2]==__field[3][3] and __field[3][1]!=' ':
+        elif field[3][1]==field[3][2]==field[3][3] and field[3][1]!=' ':
             return True
         else:
             return False
 
-    def play():
-        key=False
-        player=__player2
-        winner=player
-        while key==False:
-           if player==__player1:
-               player=__player2
+    print('Player 1: ')
+    player1=input()
+    print('Player 2: ')
+    player2=input()
+    key=False
+    player=player2
+    winner=player
+    while key==False:
+       if player==player1:
+           player=player2
+       else:
+           player=player1
+       tab_print()
+       position=[] 
+       key1=False
+       while key1==False:
+           print(player + ': 1st coordinate')
+           answer=int(input())
+           position.append(answer)
+           print(player + ': 2nd coordinate')
+           answer=int(input())
+           position.append(answer)
+           if(player==player1):
+               key1=set('x',position)
+               position.clear()
            else:
-               player=__player1
-           __print()
-           print(player + ': Check field (a,b)')
-           answer=input()
-           key1=False
-           while key1==False:
-               if(player==__player1):
-                   key1=__set('x',answer)
-               else:
-                   key1=__set('o',answer)
-           if __win()==True:
-               winner=player
-               key=True
-        print('The winner is: ' + winner)
+               key1=set('o',position)
+               position.clear()
+       if win()==True:
+           winner=player
+           key=True
+           print('The winner is: ' + winner)
+
+play()
+
+
 
             
