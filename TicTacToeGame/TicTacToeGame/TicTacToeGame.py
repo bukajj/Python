cc87080d53969c7817b695ee8e77a66ff107c259
bukajj <42746240+bukajj@users.noreply.github.com>
2019-02-02 21:30:52 +0100

    TicTacToe.1.0.2
    Ready unit tests

--- TicTacToeGame/TicTacToeGame/TicTacToeGame.py
+++ TicTacToeGame/TicTacToeGame/TicTacToeGame.py
@@ -1,1 +1,11 @@
-
+class TicTacToeGame:
+    def  __init__(self, player1, player2):
+        self.__player1=player1
+        self.__player2=player2
+        self.__field=[[' ',0,1,2],[0,' ',' ',' '],[1,' ',' ',' '],[2,' ',' ',' ']]
+    def __print():
+        pass
+    def __set(value, position):
+        pass
+    def play():
+        pass
--- TicTacToeGame/TicTacToeGame/TicTacToeTest.py
+++ TicTacToeGame/TicTacToeGame/TicTacToeTest.py
@@ -1,4 +1,31 @@
-class class1(object):
-    """description of class"""
+import TicTacToeGame
+import unittest
+
+class TicTacToeTest(unittest.TestCase):
+    def print_test():
+        test_1=TicTacToeGame('John','Mark')
+        assert test_1.__field.len()==4
+        assert test_1.__field[0].len()==4
+        assert test_1.__field[1].len()==4
+        assert test_1.__field[2].len()==4
+    def set_test():
+        test_1=TicTacToeGame('John','Mark')
+        test_1.__set('x',(0,0))
+        test_1.__set('o',(1,1))
+        test_1.__set('dads',(1,2))
+        test_1.__set('x',(3,3))
+        assert test_1.__field[0][0]=='X'
+        assert test_1.__field[0][1]==' '
+        assert test_1.__field[0][2]==' '
+        assert test_1.__field[1][0]==' '
+        assert test_1.__field[1][1]=='O'
+        assert test_1.__field[1][2]==' '
+        assert test_1.__field[2][0]==' '
+        assert test_1.__field[2][1]==' '
+        assert test_1.__field[2][2]==' '
+
+if __name__=="__main__":
+    unittest.main()
+
 
 
